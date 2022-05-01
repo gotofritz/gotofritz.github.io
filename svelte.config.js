c627f4e448529c6ef3f669da48e2f165d05546fd
gotofritz <gotofritz@users.noreply.github.com>
2022-05-01 02:47:45 +0200

    fiddle with config until github decides to make it work

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from "@sveltejs/adapter-static";
 import { mdsvex } from "mdsvex";
+const dev = process.env.NODE_ENV === "development";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
