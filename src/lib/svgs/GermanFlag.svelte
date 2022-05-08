e19ccb9abc7f7031e5bf9ede164a8e71e6e2d04d
gotofritz <gotofritz@users.noreply.github.com>
2022-05-08 23:27:11 +0200

    add german flag svg

---
+++ src/lib/svgs/GermanFlag.svelte
@@ -0,0 +1,13 @@
+<svg
+  version="1.1"
+  viewBox="0 0 5 3"
+  xmlns="http://www.w3.org/2000/svg"
+  height="1.0em"
+  width="1.67em"
+  {...$$props}
+>
+  <title>Germany</title>
+  <rect id="black_stripe" width="5" height="3" y="0" x="0" fill="#000" />
+  <rect id="red_stripe" width="5" height="2" y="1" x="0" fill="#D00" />
+  <rect id="gold_stripe" width="5" height="1" y="2" x="0" fill="#FFCE00" />
+</svg>
