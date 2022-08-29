22371c58b2d5438327e129f57b765661a06b5c5f
gotofritz <gotofritz@users.noreply.github.com>
2022-08-29 22:09:10 +0200

    fix referrer in meta tags

--- src/routes/blog/[slug].svelte
+++ src/routes/blog/[slug].svelte
@@ -62,7 +62,7 @@
     title,
   )}**?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fhyper-color-logo.svg`;
 
-  const url = `${website}/${slug}`;
+  const url = `${website}/blog/${slug}`;
 </script>
 
 <svelte:head>
